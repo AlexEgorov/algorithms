e3d7f6325ae554d1db1b62a0da7623812704d9c5
AlexEgorov <egorovalex.moi@gmail.com>
2018-02-15 22:08:17 +0300

    Added java generic insertion sort implementation.

--- java/src/com/yahorau/sorting/InsertionSort.java
+++ java/src/com/yahorau/sorting/InsertionSort.java
@@ -1,5 +1,57 @@
 package com.yahorau.sorting;
 
-public class InsertionSort {
+import java.util.*;
 
+public class InsertionSort<T extends Comparable> {
+
+    /**
+     *
+     * @param a
+     */
+    private static void sort(int a[]) {
+        // start from second element
+        for (int j = 1; j < a.length; j++) {
+            int key = a[j];
+            int i = j - 1;
+
+            // while element a[i] which is to the left of a[j] greater copy value from a[i] to a[i + 1]
+            while (i >= 0 && a[i] > key) {
+                a[i + 1] = a[i];
+                i--;
+            }
+            a[i + 1] = key;
+        }
+    }
+
+    /**
+     *
+     * @param a
+     * @param comparator
+     */
+    public void sort(List<T> a, Comparator<T > comparator) {
+        for (int j = 1; j < a.size(); j++) {
+            T key = a.get(j);
+            int i = j - 1;
+
+            while (i >= 0 && comparator.compare(a.get(i), key) > 0) {
+                a.set(i + 1, a.get(i));
+                i--;
+            }
+            a.set(i + 1, key);
+        }
+    }
+
+}
+
+class InsertionSortTest {
+    public static void main(String[] args) {
+        int n = 10;
+        List<Integer> a = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            a.add((int) (Math.random() * n));
+        }
+
+        InsertionSort<Integer> sort = new InsertionSort<>();
+        sort.sort(a, Comparator.naturalOrder());
+    }
 }
--- java/src/com/yahorau/sorting/Main.java
+++ java/src/com/yahorau/sorting/Main.java
@@ -1,8 +1,11 @@
 package com.yahorau.sorting;
 
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
     }
+
+
 }
